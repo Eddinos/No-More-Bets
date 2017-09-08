17f6d3717ece8b70434e5283099d10727b2c1dcc
Eddine Djerboua <eddine.djerboua@gmail.com>
2017-09-08 21:26:01 +0200

    first

---
+++ src/components/Napolitano.vue
@@ -0,0 +1,22 @@
+<template id="napolitano">
+  <div class="title">
+    {{name}}
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: 'napolitano',
+  data () {
+    return {
+      name: 'Al Pacino'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .title {
+    font-size: 5em;
+  }
+</style>
