c1d90cf2943512312ab4eccf51007d3885bb40fa
Eddine Djerboua <eddine.djerboua@gmail.com>
2017-12-10 13:13:38 +0100

    Go PWA
    Manifest & SW

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -8,6 +8,8 @@ var CopyWebpackPlugin = require('copy-webpack-plugin')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
+var workboxPlugin = require('workbox-webpack-plugin')
+
 
 var env = process.env.NODE_ENV === 'testing'
   ? require('../config/test.env')
@@ -96,7 +98,15 @@ var webpackConfig = merge(baseWebpackConfig, {
         to: config.build.assetsSubDirectory,
         ignore: ['.*']
       }
-    ])
+    ]),
+    // create service worker
+    new workboxPlugin({
+      globDirectory: 'dist',
+      globPatterns: ['**/*.{html,js,css}'],
+      swDest: path.join('dist', 'sw.js'),
+      clientsClaim: true,
+      skipWaiting: true,
+    })
   ]
 })
 
--- src/main.js
+++ src/main.js
@@ -10,6 +10,15 @@ Vue.config.devtools = true
 import store from './store'
 
 Vue.use(Vuex)
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then(registration => {
+      console.log('SW registered: ', registration)
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError)
+    })
+  })
+}
 
 /* eslint-disable no-new */
 new Vue({
